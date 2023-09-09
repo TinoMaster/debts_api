9d61c1932104f7e095373baeb38b0bc9fb7c642c
TinoMaster <oscarmarcos2673@gmail.com>
2023-09-09 17:02:47 +0200

    ready for production

--- services/login.services.js
+++ services/login.services.js
@@ -1,10 +1,10 @@
 const LoginController = require('../controllers/login.controllers')
 const LoginService = () => {}
 
-LoginService.login = (req, res, next) => {
+LoginService.login = (req, res) => {
   const data = req.body
   LoginController.login(data, (error, docs) => {
-    if (error?.error || error) res.status(401).json({ error: true, message: 'Usuario o contraseña incorrecta' })
+    if (error?.error || error) res.json({ error: true, message: 'Usuario o contraseña incorrecta' })
     else res.status(201).json({ success: true, data: docs })
   })
 }
