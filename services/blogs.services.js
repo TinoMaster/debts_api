826bc78a53332a21aeefdb02035b95c1f76d93ed
TinoMaster <oscarmarcos2673@gmail.com>
2023-11-10 16:29:39 +0100

    fixed send date

--- services/blogs.services.js
+++ services/blogs.services.js
@@ -7,8 +7,8 @@ BlogServices.getAllBlogs = async (req, res, next) => {
     if (err) next(err)
     else {
       const filterDocs = docs.map((blog) => {
-        const { _id, title, category, description, image, date } = blog
-        return { _id, title, category, description, image, date }
+        const { _id, title, category, description, image, createdAt } = blog
+        return { _id, title, category, description, image, date: createdAt }
       })
       res.status(200).json({ success: true, data: filterDocs })
     }
@@ -29,8 +29,8 @@ BlogServices.getBlogByCategory = async (req, res, next) => {
     if (err) next(err)
     else {
       const filterDocs = docs.map((blog) => {
-        const { _id, title, category, description, image, date } = blog
-        return { _id, title, category, description, image, date }
+        const { _id, title, category, description, image, createdAt } = blog
+        return { _id, title, category, description, image, date: createdAt }
       })
       res.status(200).json({ success: true, data: filterDocs })
     }
